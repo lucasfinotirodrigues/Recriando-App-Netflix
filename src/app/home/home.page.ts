6891bbc0a680b4448e84db63512cdafb7c8f83aa
lucas_finoti <lucasfinotirodrigues@hotmail.com>
2022-12-09 21:31:03 -0300

    "Criação de menu, logo e importação de fonte"

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,39 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage {
+
+  constructor() {}
+
+}
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { HttpClient} from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: 'home.page.html',
+//   styleUrls: ['home.page.scss'],
+// })
+// export class HomePage implements OnInit{
 
   
 
-    public stories: Array<any>= new Array<any>();
-    public movies: Array<any>= new Array<any>();
+//     public stories: Array<any>= new Array<any>();
+//     public movies: Array<any>= new Array<any>();
 
-  constructor(private httpClient: HttpClient) {}
-  ngOnInit() {  }
+//   constructor(private httpClient: HttpClient) {}
+//   ngOnInit() {  }
 
-  ionViewDidEnter(){
-    this.loadStories();
-    this.loadMovies();
-  }
+//   ionViewDidEnter(){
+//     this.loadStories();
+//     this.loadMovies();
+//   }
 
-  loadStories(){
-    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1').subscribe(data => {
-      const response: any = data;
-      this.stories = response.Search;
-    });
-  }
+//   loadStories(){
+//     this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=net&page=1').subscribe(data => {
+//       const response: any = data;
+//       this.stories = response.Search;
+//     });
+//   }
 
-  loadMovies(){
-    this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=love&page=1').subscribe(data =>{
-      const response: any = data;
-      this.movies = response.Search;
-    });
-  }
+//   loadMovies(){
+//     this.httpClient.get('http://www.omdbapi.com/?apikey=3a056eae&s=love&page=1').subscribe(data =>{
+//       const response: any = data;
+//       this.movies = response.Search;
+//     });
+//   }
 
 
-}
+// }
